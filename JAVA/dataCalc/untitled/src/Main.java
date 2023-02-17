c1dcea4a218f88a360a50c6cf93fc63342563d20
feloi <feloiwrath@gmail.com>
2023-02-17 16:14:44 -0300

    enum

---
+++ JAVA/dataCalc/untitled/src/Main.java
@@ -0,0 +1,42 @@
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDate d04 = LocalDate.parse("2023-02-04");
+        LocalDateTime d05 = LocalDateTime.parse("2023-02-04T09:50:10");
+        Instant d06 = Instant.parse("2023-02-04T09:50:10Z");
+
+        LocalDate pastWeekLocalDate = d04.minusDays(7);
+        LocalDate nextWeekLocalDate = d04.plusDays(7);
+
+        System.out.println("pastWeekLocalDate = " + pastWeekLocalDate);
+        System.out.println("NextWeekLocaldate = " + nextWeekLocalDate);
+
+        LocalDateTime pastWeekLocalDateTime = d05.minusDays(7);
+        LocalDateTime nextWeekLocalDateTime = d05.plusDays(7);
+
+        System.out.println("pastWeekLocalDateTime = " + pastWeekLocalDateTime);
+        System.out.println("NextWeekLocaldateTime = " + nextWeekLocalDateTime);
+
+        Instant pastWeekInstant = d06.minus(7, ChronoUnit.DAYS);
+        Instant nextWeekInstant = d06.plus(7, ChronoUnit.DAYS);
+
+        System.out.println("pastWeekInstant = " + pastWeekInstant);
+        System.out.println("nextWeekInstant = " + nextWeekInstant);
+
+        Duration t1 = Duration.between(pastWeekLocalDate.atStartOfDay(), d04.atStartOfDay());
+        Duration t2 = Duration.between(pastWeekLocalDateTime, d05);
+        Duration t3 = Duration.between(pastWeekInstant, d06);
+
+
+        System.out.println("Duration = " + t1.toDays());
+        System.out.println("Duration = " + t2.toDays());
+        System.out.println("Duration = " + t3.toDays());
+
+
+    }
+}
